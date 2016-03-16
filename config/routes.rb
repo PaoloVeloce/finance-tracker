d6f673ebae0653b9458de80c007c9c9fb50fafed
Paolo Veloce <Pablo.veloce@gmail.com>
2016-03-16 05:25:04 +0000

    add view to my_friends with table and add routes resources for users except show and friendships

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+
 
 end
